8adb4e577e2d3567b1bc14851c566c6bea493330
shivam-rallyhealth <shivam-rallyhealth>
2022-05-16 19:15:35 +0530

    Added Marker

--- App.tsx
+++ App.tsx
@@ -73,10 +73,7 @@ const App = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
+          <Section title="Step One">Content Megazord</Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
           </Section>
